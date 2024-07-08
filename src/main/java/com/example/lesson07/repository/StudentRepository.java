b3862ba95541ac20b17fc242dbe7992fa2814b86
DESKTOP-AN805P4\user <test@test.com>
2024-07-08 18:52:09 +0900

    2024-07-08 수업

--- src/main/java/com/example/lesson07/repository/StudentRepository.java
+++ src/main/java/com/example/lesson07/repository/StudentRepository.java
@@ -3,6 +3,8 @@ package com.example.lesson07.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.lesson07.entity.StudentEntity;
 
@@ -15,7 +17,7 @@ public interface StudentRepository extends JpaRepository<StudentEntity, Integer>
 	// public void delete(StudentEntity students)
 	// public List<StudentEntity> findAll()
 	
-	// ex02/select1 - JPQL 문법
+	// ex02/select1 - JPQL문법 메소드명을 만들면 규칙에 따라 자동으로 실행해줌
 	public List<StudentEntity> findAllByOrderByIdDesc();
 	public List<StudentEntity> findTop3ByOrderByIdDesc();
 	public List<StudentEntity> findByName(String name11111);
@@ -24,5 +26,12 @@ public interface StudentRepository extends JpaRepository<StudentEntity, Integer>
 	public List<StudentEntity> findByEmailContains(String keyword);
 	public List<StudentEntity> findByNameStartingWith(String keyword);
 	public List<StudentEntity> findByIdBetween(int startId, int endId);
+	
+	// ex02/select2 native query 내가직접쿼리문작성
+	//@Query(value = "select * from `new_student` where `dreamJob` = :dreamJob", nativeQuery = true) // nativeQuery = true => DB에 직접 SQL 쿼리 수행
+	
+	// ex02/select2-1 => JPQL(Entity를 조회:쿼리는 back에서 알아서 해줌) - SQL query 아님!
+	@Query(value = "select s from StudentEntity s where s.dreamJob = :dreamJob")  // nativeQuery = false
+	public List<StudentEntity> findByDreamJob(@Param("dreamJob") String dreamJob);
 
 }
